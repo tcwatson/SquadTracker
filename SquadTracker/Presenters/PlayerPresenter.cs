07fe5335a2306f2b5d38c25db8081a28dea5e2ad
Tyler Watson <10892630+tcwatson@users.noreply.github.com>
2022-03-15 00:03:02 -0500

    WIP Create player view and presenter

---
+++ SquadTracker/Presenters/PlayerPresenter.cs
@@ -0,0 +1,11 @@
+﻿using Blish_HUD.Graphics.UI;
+using Torlando.SquadTracker.Models;
+using Torlando.SquadTracker.Views;
+
+namespace Torlando.SquadTracker.Presenters
+{
+    internal class PlayerPresenter : Presenter<PlayerView, Player>
+    {
+        public PlayerPresenter(PlayerView view, Player model) : base(view, model) { }
+    }
+}
---
+++ SquadTracker/Views/PlayerView.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Blish_HUD.Controls;
+using Blish_HUD.Graphics.UI;
+using Microsoft.Xna.Framework;
+using Torlando.SquadTracker.Models;
+using Torlando.SquadTracker.Presenters;
+
+namespace Torlando.SquadTracker.Views
+{
+    class PlayerView : View
+    {
+    }
+}
