66cf4c9a9eed908fcb11b22cd4922fbc032f8026
Tyler Watson <10892630+tcwatson@users.noreply.github.com>
2022-03-15 20:07:05 -0500

    Add current and former squad member panels

--- SquadTracker/SquadPanel/SquadPanelView.cs
+++ SquadTracker/SquadPanel/SquadPanelView.cs
@@ -1,5 +1,6 @@
 ﻿using Blish_HUD.Controls;
 using Blish_HUD.Graphics.UI;
+using Microsoft.Xna.Framework;
 
 namespace Torlando.SquadTracker.SquadPanel
 {
@@ -10,6 +11,9 @@ namespace Torlando.SquadTracker.SquadPanel
         private Menu _menuCategories;
         private MenuItem _squadMembersMenu;
         private MenuItem _squadRolesMenu;
+        private FlowPanel _squadMembersPanel;
+        private FlowPanel _formerSquadMembersPanel;
+        private StandardButton _clearFormerSquadButton;
         #endregion
         protected override void Build(Container buildPanel)
         {
@@ -31,6 +35,38 @@ namespace Torlando.SquadTracker.SquadPanel
             _squadMembersMenu.Select();
 
             _squadRolesMenu = _menuCategories.AddMenuItem("Squad Roles");
+            _squadMembersPanel = new FlowPanel
+            {
+                FlowDirection = ControlFlowDirection.LeftToRight,
+                ControlPadding = new Vector2(8, 8),
+                Parent = buildPanel,
+                Location = new Point(_menuCategories.Right + 10, _menuCategories.Top),
+                CanScroll = true,
+                Size = new Point(buildPanel.Width - _menuCategories.Width - 5, 530), //
+                Title = "Current Squad Members",
+                ShowBorder = true
+            };
+            _formerSquadMembersPanel = new FlowPanel
+            {
+                FlowDirection = ControlFlowDirection.LeftToRight,
+                ControlPadding = new Vector2(8, 8),
+                Parent = buildPanel,
+                Location = new Point(_menuCategories.Right + 10, _squadMembersPanel.Bottom + 10),
+                CanScroll = true,
+                Size = new Point(buildPanel.Width - _menuCategories.Width - 5, 150),
+                Title = "Former Squad Members",
+                ShowBorder = true
+            };
+            _clearFormerSquadButton = new StandardButton
+            {
+                Parent = buildPanel,
+                Text = "Clear",
+                Location = new Point(_formerSquadMembersPanel.Right - 135, _formerSquadMembersPanel.Top + 5)
+            };
+            //_clearFormerSquadButton.Click += delegate
+            //{
+            //    _playerCollection.ClearFormerPlayers();
+            //};
         }
     }
 }
